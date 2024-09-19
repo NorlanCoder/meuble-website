c4d910a3fe586d7295150dfaa10e8c4d560641b5
Romuad-Zoungla <missimahouzoun@gmail.com>
2024-09-19 18:03:54 +0100

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+
+function loadHTML(filePath, elementID) {
+    fetch(filePath)
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById(elementID).innerHTML = data;
+      })
+      .catch(error => console.error('Error loading HTML:', error));
+  }
+  
+  // Charger les pages
+
+  loadHTML('/pages/footer.html', 'footer');
+  
