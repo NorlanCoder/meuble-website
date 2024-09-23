66765dc600882be5a31ece206178c773571163e6
Darking <zsamuel737@gmail.com>
2024-09-23 08:51:48 +0100

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
     extend: {
       colors: {
         'orange' : '#FB923C',
+      },
+      backgroundImage: {
+        'gallery1': "url('/src/assets/img-gallery1.svg')",
+        'gallery2': "url('/src/assets/img-gallery2.svg')",
+        'gallery3': "url('/src/assets/img-gallery3.svg')",
+        'gallery4': "url('/src/assets/img-gallery4.svg')",
+        'gallery5': "url('/src/assets/img-gallery5.svg')",
+        'gallery6': "url('/src/assets/img-gallery6.svg')",
+        
       }
     },
     
